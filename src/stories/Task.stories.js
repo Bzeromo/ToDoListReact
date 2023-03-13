23190c7105463c2ac9a19ab4ce19d366d6cfaddf
Bzeromo <dudrb5260@naver.com>
2023-03-13 17:59:18 +0900

    first

---
+++ src/stories/Task.stories.js
@@ -0,0 +1,14 @@
+import Task from "../components/Task"
+
+export default {
+    title: 'Component/Task',
+    component: Task,
+}
+
+export const Default = () => {
+    const task = {
+        content: '출근하기',
+        complete: false
+    }
+    return <Task content={task.content} complete={task.complete} />;
+}
